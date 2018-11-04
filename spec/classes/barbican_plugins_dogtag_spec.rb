436a89e21658386cbb0d762658687eafb6df6f06
Tobias Urdin <tobias.urdin@binero.se>
2018-11-04 13:37:40 +0100

    Convert spec testing to use rspec-puppet-facts
    Change-Id: I2f385c42020c7ae862d2385a807f9a1c7fb8dc68

--- spec/classes/barbican_plugins_dogtag_spec.rb
+++ spec/classes/barbican_plugins_dogtag_spec.rb
@@ -20,86 +20,76 @@
 require 'spec_helper'
 
 describe 'barbican::plugins::dogtag' do
-
-  let :facts do
-    @default_facts.merge(
-      {
-        :osfamily   => 'RedHat',
-        :os_workers => '7',
-      }
-    )
-  end
-
   let :default_params do
     {
-      :dogtag_plugin_ensure_package       => 'present',
-      :dogtag_plugin_pem_path             => '<SERVICE DEFAULT>',
-      :dogtag_plugin_dogtag_host          => '<SERVICE DEFAULT>',
-      :dogtag_plugin_dogtag_port          => '<SERVICE DEFAULT>',
-      :dogtag_plugin_nss_db_path          => '<SERVICE DEFAULT>',
-      :dogtag_plugin_simple_cmc_profile   => '<SERVICE DEFAULT>',
-      :dogtag_plugin_ca_expiration_time   => '<SERVICE DEFAULT>',
-      :dogtag_plugin_plugin_working_dir   => '<SERVICE DEFAULT>',
-      :global_default                     => false,
+      :dogtag_plugin_ensure_package     => 'present',
+      :dogtag_plugin_pem_path           => '<SERVICE DEFAULT>',
+      :dogtag_plugin_dogtag_host        => '<SERVICE DEFAULT>',
+      :dogtag_plugin_dogtag_port        => '<SERVICE DEFAULT>',
+      :dogtag_plugin_nss_db_path        => '<SERVICE DEFAULT>',
+      :dogtag_plugin_simple_cmc_profile => '<SERVICE DEFAULT>',
+      :dogtag_plugin_ca_expiration_time => '<SERVICE DEFAULT>',
+      :dogtag_plugin_plugin_working_dir => '<SERVICE DEFAULT>',
+      :global_default                   => false,
     }
   end
 
-  [{
-      :dogtag_plugin_nss_password         => 'password',
-   },
-   {
-      :dogtag_plugin_pem_path             => 'path_to_pem_file',
-      :dogtag_plugin_dogtag_host          => 'dogtag_host',
-      :dogtag_plugin_dogtag_port          => '1234',
-      :dogtag_plugin_nss_db_path          => 'path_to_nss_db',
-      :dogtag_plugin_nss_password         => 'password',
-      :dogtag_plugin_simple_cmc_profile   => 'caServerCert',
-      :dogtag_plugin_ca_expiration_time   => '100',
-      :dogtag_plugin_plugin_working_dir   => 'path_to_working_dir',
-      :global_default                     => true,
-    }
-  ].each do |param_set|
+  shared_examples 'barbican::plugins::dogtag' do
+    [{
+       :dogtag_plugin_nss_password => 'password',
+     },
+     {
+       :dogtag_plugin_pem_path           => 'path_to_pem_file',
+       :dogtag_plugin_dogtag_host        => 'dogtag_host',
+       :dogtag_plugin_dogtag_port        => '1234',
+       :dogtag_plugin_nss_db_path        => 'path_to_nss_db',
+       :dogtag_plugin_nss_password       => 'password',
+       :dogtag_plugin_simple_cmc_profile => 'caServerCert',
+       :dogtag_plugin_ca_expiration_time => '100',
+       :dogtag_plugin_plugin_working_dir => 'path_to_working_dir',
+       :global_default                   => true,
+      }
+    ].each do |param_set|
+      context "when #{param_set == {} ? "using default" : "specifying"} class parameters" do
+        let :param_hash do
+          default_params.merge(param_set)
+        end
 
-    describe "when #{param_set == {} ? "using default" : "specifying"} class parameters" do
+        let :params do
+          param_set
+        end
 
-      let :param_hash do
-        default_params.merge(param_set)
-      end
+        it { should contain_package('dogtag-client').with(
+          :ensure => param_hash[:dogtag_plugin_ensure_package],
+          :name   => 'pki-base',
+          :tag    => ['openstack', 'barbican-package'],
+        )}
 
-      let :params do
-        param_set
-      end
-
-      it { is_expected.to contain_package('dogtag-client').with(
-        'ensure' => param_hash[:dogtag_plugin_ensure_package],
-        'tag'    => ['openstack', 'barbican-package'],
-      ) }
-
-      it 'is_expected.to set dogtag parameters' do
-        is_expected.to contain_barbican_config('dogtag_plugin/pem_path')\
-          .with_value(param_hash[:dogtag_plugin_pem_path])
-        is_expected.to contain_barbican_config('dogtag_plugin/dogtag_host')\
-          .with_value(param_hash[:dogtag_plugin_dogtag_host])
-        is_expected.to contain_barbican_config('dogtag_plugin/dogtag_port')\
-          .with_value(param_hash[:dogtag_plugin_dogtag_port])
-        is_expected.to contain_barbican_config('dogtag_plugin/nss_db_path')\
-          .with_value(param_hash[:dogtag_plugin_nss_db_path])
-        is_expected.to contain_barbican_config('dogtag_plugin/nss_password')\
-          .with_value(param_hash[:dogtag_plugin_nss_password]).with_secret(true)
-        is_expected.to contain_barbican_config('dogtag_plugin/simple_cmc_profile')\
-          .with_value(param_hash[:dogtag_plugin_simple_cmc_profile])
-        is_expected.to contain_barbican_config('dogtag_plugin/ca_expiration_time')\
-          .with_value(param_hash[:dogtag_plugin_ca_expiration_time])
-        is_expected.to contain_barbican_config('dogtag_plugin/plugin_working_dir')\
-          .with_value(param_hash[:dogtag_plugin_plugin_working_dir])
-        is_expected.to contain_barbican_config(
-          'secretstore:dogtag/secret_store_plugin') \
-          .with_value('dogtag_plugin')
-        is_expected.to contain_barbican_config(
-          'secretstore:dogtag/global_default') \
-          .with_value(param_hash[:global_default])
+        it {
+          should contain_barbican_config('dogtag_plugin/pem_path').with_value(param_hash[:dogtag_plugin_pem_path])
+          should contain_barbican_config('dogtag_plugin/dogtag_host').with_value(param_hash[:dogtag_plugin_dogtag_host])
+          should contain_barbican_config('dogtag_plugin/dogtag_port').with_value(param_hash[:dogtag_plugin_dogtag_port])
+          should contain_barbican_config('dogtag_plugin/nss_db_path').with_value(param_hash[:dogtag_plugin_nss_db_path])
+          should contain_barbican_config('dogtag_plugin/nss_password').with_value(param_hash[:dogtag_plugin_nss_password]).with_secret(true)
+          should contain_barbican_config('dogtag_plugin/simple_cmc_profile').with_value(param_hash[:dogtag_plugin_simple_cmc_profile])
+          should contain_barbican_config('dogtag_plugin/ca_expiration_time').with_value(param_hash[:dogtag_plugin_ca_expiration_time])
+          should contain_barbican_config('dogtag_plugin/plugin_working_dir').with_value(param_hash[:dogtag_plugin_plugin_working_dir])
+          should contain_barbican_config('secretstore:dogtag/secret_store_plugin').with_value('dogtag_plugin')
+          should contain_barbican_config('secretstore:dogtag/global_default').with_value(param_hash[:global_default])
+        }
       end
     end
   end
-end
 
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts( :os_workers => '7' ))
+      end
+
+      it_behaves_like 'barbican::plugins::dogtag'
+    end
+  end
+end
