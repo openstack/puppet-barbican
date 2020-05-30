60d3b6826668c96f538aaea4db72b93abccad078
Thomas Goirand <zigo@debian.org>
2020-05-30 03:02:49 +0200

    Debian: do not configure gunicorn-config.py
    Debian isn't using gunicorn for Barbican, but UWSGI.
    Without this patch, this module simply wouldn't work
    for Debian.
    
    Change-Id: Iaafc7f4b1499df471a35410228a7ad86938e94cc

--- manifests/api.pp
+++ manifests/api.pp
@@ -479,11 +479,14 @@ class barbican::api (
       tag        => 'barbican-service',
     }
 
-    file_line { 'Modify bind_port in gunicorn-config.py':
-      path  => '/etc/barbican/gunicorn-config.py',
-      line  => "bind = '${bind_host}:${bind_port}'",
-      match => '.*bind = .*',
-      tag   => 'modify-bind-port',
+    # Debian is using UWSGI, not gunicorn
+    if $::os_package_type != 'debian' {
+      file_line { 'Modify bind_port in gunicorn-config.py':
+        path  => '/etc/barbican/gunicorn-config.py',
+        line  => "bind = '${bind_host}:${bind_port}'",
+        match => '.*bind = .*',
+        tag   => 'modify-bind-port',
+      }
     }
 
   } elsif $service_name == 'httpd' {
