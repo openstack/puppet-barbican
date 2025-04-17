073977f158098138a06f754bdbe6a23d238481d6
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-04-17 23:40:09 +0900

    Drop deprecated api_config
    It was deprecated in favor of the barbican_config parameter[1].
    
    [1] 0a1c988b0f4344cb8fc7872d605276475960c67e
    
    Change-Id: I8a42a4377b1d06aae25eef6324c4a1b59c45773b

--- manifests/config.pp
+++ manifests/config.pp
@@ -23,26 +23,15 @@
 # [*api_paste_ini_config*]
 # (optional) Allow configuration of barbican-api-paste.ini configurations.
 #
-# DEPRECATED PARAMETERS
-#
-# [*api_config*]
-# (optional) Allow configuration of barbican.conf configurations.
-#
 #   NOTE: The configuration MUST NOT be already handled by this module
 #   or Puppet catalog compilation will fail with duplicate resources.
 #
 class barbican::config (
   Hash $barbican_config      = {},
   Hash $api_paste_ini_config = {},
-  Optional[Hash] $api_config = undef,
 ) {
 
   include barbican::deps
-
-  if $api_config {
-    warning('The api_config parametr is deprecated. Use the barbican_config parameter instead')
-    create_resources('barbican_config', $api_config)
-  }
 
   create_resources('barbican_config', $barbican_config)
   create_resources('barbican_api_paste_ini', $api_paste_ini_config)
